e96e63f8f7dadb339cf109875f95eb5a84782a4a
PunitVJain <putwind@gmail.com>
2021-06-27 00:46:41 +0530

    commit

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app  =  Flask(__name__)
 
 @app.route('/')
-def home():
+def login():
     return render_template('login.html')
 
 if __name__ == '__main__':
