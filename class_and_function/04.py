af19a5082d764273a10012e2b8bdb3337edef3f4
PunitVJain <putwind@gmail.com>
2021-06-27 00:24:18 +0530

    commit

--- class_and_function/04.py
+++ class_and_function/04.py
@@ -1,2 +1,13 @@
-#  start with pandas
-import pandas as pd
+#  start with classes in python.
+
+class Sample:
+
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+
+
+
---
+++ flask/Config/Config.py
@@ -0,0 +1,4 @@
+#  DB configuration
+
+class Config:
+    pass
