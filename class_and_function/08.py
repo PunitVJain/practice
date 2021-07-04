636cce17fa581f7fe81228a5ad0c793d9931085d
PunitVJain <putwind@gmail.com>
2021-07-04 17:46:10 +0530

    commit

--- class_and_function/08.py
+++ class_and_function/08.py
@@ -4,3 +4,8 @@ print('Punit Jain.')
 print('''My name is 'Punit Jain'. 
         I am from 'Malkapur'. 
         It's awesome working with 'PYTHON'.''')
+name = 'Punit Jain' #  string variable.
+print(f'My name is "{name}".')# f string.
+print('My name is ', name) # just print.
+print('Name is ' +name) # just print.
+print('*' * 100)
