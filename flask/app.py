e3888d58465b503f5c68cdc73c58dc75d64715d0
PunitVJain <putwind@gmail.com>
2021-07-05 11:19:06 +0530

    commit

--- flask/app.py
+++ flask/app.py
@@ -8,5 +8,11 @@ app  =  Flask(__name__)
 def login():
     return render_template('login.html')
 
+@app.route('/login_creds')
+def login_creds():
+    pass
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
