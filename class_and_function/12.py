236e90fc5030dd4a69a0a803ce83304f290d1a17
PunitVJain <putwind@gmail.com>
2021-07-10 11:46:55 +0530

    commit

---
+++ class_and_function/12.py
@@ -0,0 +1,9 @@
+#  python for this Satarday & Sunday
+#  needed to write the methods to write the outline of the tree.
+#  create decorators & generator in python.
+#  try to use the stattic methods & class methods in python.
+#  also try to learn in MRO as well.
+#  API needed to create in python.
+
+class Sample:
+    pass
