6c335dabc3a2d7ddcc663dc15eb79e084857c936
PunitVJain <putwind@gmail.com>
2021-07-17 18:42:29 +0530

    commit

--- class_and_function/16.py
+++ class_and_function/16.py
@@ -1,2 +1,36 @@
-#  python function to send the requests
+#  python function.
+
+mystr = 'My name is Punit Jain'
+
+# O/p = 'A IDNI evolI'
+
+
+def revspace(mystr):
+    index_No = [] # index number of the space
+    for iteam, value in enumerate(mystr):
+        if value == ' ':
+            index_No.append(iteam)
+    mstr = mystr.replace(' ', '') # replaces the ' ' 
+    revstr = list(mstr[::-1]) # reverse the string & converted to list
+    for ind in range(len(index_No)):
+        revstr.insert(index_No[ind], ' ') # inserted the space at the index_no list elements
+    revstr = ''.join(revstr) # join & converted to string
+    return revstr
+print(revspace(mystr))
+
+
+# reverse the string without predefined functions & slicing.
+
+def reverse_string(mystr):
+    index = len(mystr)
+    mylist = []
+    while index:
+        index -= 1
+        mylist.append(mystr[index])
+    return ''.join(mylist)
+mystr = 'PunitJain'
+
+print(reverse_string(mystr))
+
+
 
