39ae44a1b44c958981cf3963bef8ae1ba9cc6b3b
PunitVJain <putwind@gmail.com>
2021-07-18 13:48:05 +0530

    commit

--- class_and_function/17.py
+++ class_and_function/17.py
@@ -5,5 +5,11 @@ class Sample:
     def __init__(self) -> None:
         pass
 
-    def revs_str(self, mystr):
-        pass
+    def revs_str(self):
+        return 'Punit Jain'
+    
+    def mymain(self):
+        sam1 =  Sample()
+        print(sam1.revs_str())
+sam2 =  Sample()
+sam2.mymain()
