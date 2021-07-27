2916c80a7041bfc8642451dcfcd9e1071d5922d6
PunitVJain <putwind@gmail.com>
2021-07-27 16:48:16 +0530

    commit

--- class_and_function/22.py
+++ class_and_function/22.py
@@ -1,1 +1,8 @@
-#  python basics 
+#  python basics data-types in python.
+# mystr = "Indian Army" --> string in python.s
+# mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'P', 'u', 'n', 'i', 't'] --> list in python.
+# mydict = {'First': 'Punit', 'Last': 'Jain'} --> Dictionary in python. 
+# myset = {2, 4, 6, 8, 10} --> set in python
+# mytup = (1, 2, 3, 4, 5, 6, 7, 8, 9) -- > tuple in python. 
+# obool  = True --> boolen value.
+# int & float --> number type.
