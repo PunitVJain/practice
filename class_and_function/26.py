9146f5a947507ac95476fdf1fdb18ce0ecbe3de8
PunitVJain <putwind@gmail.com>
2021-08-05 22:26:58 +0530

    commit

--- class_and_function/26.py
+++ class_and_function/26.py
@@ -4,10 +4,13 @@ class Dog:
     # class of the flask.
     def __init__(self, name: str) -> str:
         self.name = name
+        # attribute of the class  'self'
+
 
     def bark(self):
         # method of the Dog class.
         return ('bark......')
     
 dog = Dog("Punit")
-print(dog.bark())
+
+cat = Dog("Jain")
---
+++ class_and_function/27.py
@@ -0,0 +1,18 @@
+class Solution:
+	def lengthOfLongestSubstring(self, s: str) -> int:
+
+
+		substring = ''
+		max_ = 0
+		for char in s:
+			if char in substring:
+				substring = substring.split(char)[1]
+			substring += char
+			max_ = len(substring) if len(substring) > max_ else max_
+		return max_
+sol = Solution()
+print(sol.lengthOfLongestSubstring("abcabcbb"))
+# a --> ss --> a -->  
+
+
+# "abcabcbb" -->  
