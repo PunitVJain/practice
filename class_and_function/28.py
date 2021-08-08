5d526dd3768ea1f6245bfa943eeaab7ea9277796
PunitVJain <putwind@gmail.com>
2021-08-08 23:15:48 +0530

    commit

---
+++ class_and_function/28.py
@@ -0,0 +1,13 @@
+#  advance python 
+
+# list
+
+mylist =  [] #  this will create the blank list
+#  
+mydict1  = dict() #  this will create a blank dictionary
+
+mydict2 = {} # this will create a blank dictionary
+
+print(type(mydict1))
+
+
