cac4fc4cf976e102cc1cf52813ec1b389e2cdcc0
PunitVJain <putwind@gmail.com>
2021-08-25 23:15:53 +0530

    commit

--- class_and_function/31.py
+++ class_and_function/31.py
@@ -4,3 +4,12 @@
 #  
 # python developer 
 
+class Sample(object):
+
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def my_name(self):
+        return "Punit Jain"
+Sa = Sample()
+print(Sa.my_name())
