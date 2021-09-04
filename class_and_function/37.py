f6b8e316787d7766fde9334cc86f9c3c9cdc8264
PunitVJain <putwind@gmail.com>
2021-09-04 14:39:34 +0530

    commit

--- class_and_function/37.py
+++ class_and_function/37.py
@@ -1,2 +1,22 @@
 #  python developer.
 
+# Write a function to find the longest common prefix 
+# string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        if len(strs) == 0: return ""
+        elif len(strs) == 1: return strs[0]
+        min_s = min(strs, key= len)
+        max_s = max(strs, key= len)
+        for iteam, value in enumerate(min_s):
+            if max_s[iteam] != value: 
+                return min_s[:iteam]
+        return min_s
+
+so = Solution()
+mystr = ["Vimal"]
+print(so.longestCommonPrefix(mystr))
+
+
---
+++ class_and_function/38.py
@@ -0,0 +1,5 @@
+#  find the min & max method inn python.
+
+mylist = ["PUNIT", "Vinod", "india", "Malkapur"]
+print(min(mylist, key=len))
+print(max(mylist, key=len))
