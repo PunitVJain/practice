78a10f49b425b26e46081d1b9652a6e61edc9e37
PunitVJain <putwind@gmail.com>
2021-09-11 13:30:29 +0530

    commit

--- class_and_function/46.py
+++ class_and_function/46.py
@@ -1,2 +1,5 @@
 # leetcode problems:
 # Python.
+
+mylist = [[10, 20, 30, 40],[20, 30, 40, 50],[60, 70, 80, 90],[100, 120, 130, 20],[30, 40, 50]]
+print(sorted(mylist, key= lambda iteam: iteam[-1]))
