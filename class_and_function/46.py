6543d4aab131f9f0690abf3e29dfe8860bb83a44
PunitVJain <putwind@gmail.com>
2021-09-16 23:54:44 +0530

    commit

--- class_and_function/46.py
+++ class_and_function/46.py
@@ -2,7 +2,7 @@
 # Python.
 
 mylist = [[10, 20, 30, 40],[20, 30, 40, 50],[60, 70, 80, 90],[100, 120, 130, 20],[30, 40, 50]]
-print(sorted(mylist, key= lambda iteam: iteam[-1]))
+#print(sorted(mylist, key= lambda iteam: iteam[-1]))
 
 def rev_str(mystr):
     return ' '.join(mystr.split()[::-1])
