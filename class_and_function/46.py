f8490a94c8502259c88f532d7d34e287a66eb864
PunitVJain <putwind@gmail.com>
2021-09-16 23:17:44 +0530

    commit

--- class_and_function/46.py
+++ class_and_function/46.py
@@ -4,3 +4,8 @@
 mylist = [[10, 20, 30, 40],[20, 30, 40, 50],[60, 70, 80, 90],[100, 120, 130, 20],[30, 40, 50]]
 print(sorted(mylist, key= lambda iteam: iteam[-1]))
 
+def rev_str(mystr):
+    return ' '.join(mystr.split()[::-1])
+
+mystr = "Punit Vinod Jain"
+print(rev_str(mystr))
