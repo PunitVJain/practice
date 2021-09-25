4522dacc95078fe93dedef114e624652a54a1ad4
PunitVJain <putwind@gmail.com>
2021-09-25 18:08:44 +0530

    commit

--- class_and_function/47.py
+++ class_and_function/47.py
@@ -6,13 +6,8 @@ import json
 
 #mylist =  json.dumps(data)
 class Sample:
-
-    def __init__(self, file) -> None:
-
-        self.file = file
-        
-    
-    def add_index_timestamp(self, data):
+  
+    def add_index_timestamp(self,data):
         ct = datetime.datetime.now()
         for iteam, value in enumerate(data):
             value["index"] = iteam
@@ -23,19 +18,15 @@ class Sample:
         pass
     
     def read_json_file(self, file):
-        with open(file, "rW") as file:
+        with open(file, 'r+') as file:
             file = json.loads(file.read())
-
-        pass
+            return file
 
 def main():
-    data = [{"firstname":"Punit", "lastname":"Jain", "mobileno":"8237914234", "location":"Malkapur"},
-            {"firstname":"Rakhi", "lastname":"Jain", "mobileno":"7038680983", "location":"Pune"},
-            {"firstname":"Rekha", "lastname":"Jain", "mobileno":"9689353311", "location":"Mumbai"},
-            {"firstname":"Shravani", "lastname":"Jain", "mobileno":"9768777787", "location":"Banglore"}]
+    data = "/media/putwind/Card1/Movies/Desktop/Python Practic Programmes/Python_Study/practice/class_and_function/file.json"
 
     sa_one = Sample()
-    print(sa_one.add_index_timestamp(data))
+    print(sa_one.add_index_timestamp(sa_one.read_json_file(data)))
 
 if __name__ == "__main__":
     main()
