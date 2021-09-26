9fe3aee2fe42deff9250947bb685641ffd5937e7
PunitVJain <putwind@gmail.com>
2021-09-26 11:19:15 +0530

    commit

---
+++ class_and_function/51.py
@@ -0,0 +1,16 @@
+#  file modification
+
+class Example:
+
+    def __init__(self, file_loc) -> None:
+        self.file_loc = file_loc
+
+    def read_file(self):
+        pass
+
+    def add_fields_file(self):
+        pass
+
+    def delete_fields_file(self):
+        pass
+    
