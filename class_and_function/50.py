6377762dd5afb4d86528ef7d772be220cbc51560
PunitVJain <putwind@gmail.com>
2021-09-26 14:39:54 +0530

    commit

--- class_and_function/50.py
+++ class_and_function/50.py
@@ -1,1 +1,6 @@
 # python to connect MySQl with Python.
+import pymysql
+
+mydb =  pymysql.connect(host="20.204.146.108",user="root",passwd="Expo@123",database="dcb",port = 3306)
+cursor = mydb.cursor()
+print(mydb)
