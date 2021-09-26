afe035c938933a3f2f2cefaccc69b90897c1d4b8
PunitVJain <putwind@gmail.com>
2021-09-26 23:23:31 +0530

    commit

--- DATA_Structure_and_Algorithms/01.py
+++ DATA_Structure_and_Algorithms/01.py
@@ -1,2 +1,15 @@
 #  data structure with python.
 #  Algorithms with Practice.
+
+#  stack in python 
+
+# 1. Stack works on the Principle of last in first out(LIFO).
+# 2. Stack has two functions as PUSH() & POP()
+
+
+class Stack:
+
+    def push():
+        pass
+
+Stack.push()
