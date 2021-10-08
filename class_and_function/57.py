521b405e3412c07f75cba6cd4e2aee8166cec1bb
PunitVJain <putwind@gmail.com>
2021-10-08 23:10:45 +0530

    commit

--- class_and_function/57.py
+++ class_and_function/57.py
@@ -1,2 +1,10 @@
-#  python with logic building.
+# read csv file.
 
+class CsvFile(object):
+
+    def readfile(self, fileloc):
+        with open(fileloc, 'r+') as file:
+            data = file.read()
+            return data
+cf = CsvFile()
+print(cf.readfile("/home/putwind/Desktop/MOCK_DATA.csv")) 
