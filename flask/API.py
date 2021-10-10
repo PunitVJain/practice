c5e594612e0d0318ae9ef3800325b890f0c43fe7
PunitVJain <putwind@gmail.com>
2021-10-10 21:51:52 +0530

    commit

--- flask/API.py
+++ flask/API.py
@@ -2,13 +2,14 @@
 from flask import Flask, request
 
 
+
 app = Flask(__name__)
 @app.route('/api/login', methods=["GET", "POST"])
 def login_page():
     _json = request.json
-    username = _json["username"]
-    password = _json["password"]
-    return  {"password": password, "username": username}
+    _username = _json["username"]
+    _password = _json["password"]
+    return  {"password": _password, "username": _username}
 
 
 if __name__ == "__main__":
