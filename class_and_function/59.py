9475214b1002994e1d84b29cfaf137a93e1f21a5
PunitVJain <putwind@gmail.com>
2021-10-21 23:12:43 +0530

    commit

--- class_and_function/59.py
+++ class_and_function/59.py
@@ -2,4 +2,9 @@
 #  decorator are the function which takes the function &
 #  enhances the functionality of the existing function.
 
+#  decorators start with "@" in python.
+#  functions are the first class citisions.
+
+# due to the functions are first class citisions in python.
+# stack DS with python.
 
