3f612af17923538d3e2e220201331684f4cbbbd7
PunitVJain <putwind@gmail.com>
2021-11-06 22:35:17 +0530

    decorator

--- class_and_function/05.py
+++ class_and_function/05.py
@@ -1,11 +1,14 @@
 #  decorators in python
+# needed to create decorator as before name it must return 'My name is'.
+#  example of decorators.
 
+def decfun(name):
+    def wrapper(*args, **kwargs):
+        return "My name is " + name()
+    return wrapper
+
+@decfun
 def name():
     return 'Punit Jain'
 
-def decfun():
-    pass
-
-
-# needed to create decorator as before name it must return 'My name is'.
-
+print(name())
