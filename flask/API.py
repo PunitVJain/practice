45d6606805bda95a19a10a68994da26d4574a3de
PunitVJain <putwind@gmail.com>
2021-11-14 12:08:58 +0530

    rest api

--- flask/API.py
+++ flask/API.py
@@ -1,15 +1,26 @@
 
 from flask import Flask, request
+from flask_restful import Api, Resource
 
 
 
 app = Flask(__name__)
+api = Api(app)
+
 @app.route('/api/login', methods=["GET", "POST"])
 def login_page():
     _json = request.json
     _username = _json["username"]
     _password = _json["password"]
     return  {"password": _password, "username": _username}
+
+
+class Sample(Resource):
+
+    def get(self):
+        return "Hello World!"
+
+api.add_resource(Sample, "/api/helloworld")
 
 
 if __name__ == "__main__":
