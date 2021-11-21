5350f6bf0c5498a69872f122daf090949d4897f7
PunitVJain <putwind@gmail.com>
2021-11-21 17:37:13 +0530

    class added 63

--- class_and_function/63.py
+++ class_and_function/63.py
@@ -1,4 +1,7 @@
 #  python for Punit Jain.
+from numpy.random.mtrand import sample
+
+
 class Sample(object):
 
     def __init__(self) -> None:
@@ -9,4 +12,16 @@ if __name__ == "__main__":
 
 
 #"Checking, Is it working or not? "
+
+# python with Punit Jain.
+
+class Example(Sample):
+
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def name(self):
+        pass
+    
+
 
