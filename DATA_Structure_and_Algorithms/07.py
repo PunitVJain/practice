3e7b26662aaee9f41311a2dfe1a11672da6f8253
Punit <punit.jain@exponentia.ai>
2021-11-21 11:50:44 +0530

    link list in DSA in python

---
+++ DATA_Structure_and_Algorithms/07.py
@@ -0,0 +1,15 @@
+#  link list
+
+
+class Node:
+
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next_add = None
+
+class LinkedList:
+
+    def __init__(self) -> None:
+        self.head = None
+
+    
