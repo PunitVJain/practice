d0c4a48e37a65ab92d81fce171b0aa91982b3b03
PunitVJain <putwind@gmail.com>
2021-11-26 00:54:52 +0530

    commit

--- class_and_function/64.py
+++ class_and_function/64.py
@@ -1,4 +1,22 @@
 #  Python leet code 
-# Given an integer n, 
-# count the total number of digit 1 appearing in 
-# all non-negative integers less than or equal to n.
+#
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+
+
+
+def rom_to_num(rom:str):
+    roman_nums = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    rom_list = list(rom)
+    print(rom_list)
+    nums = [roman_nums.get(n, n) for n in rom_list]
+    print(nums)
+    
+
+
+
+
+
+
+
+rom_to_num("XIV")
