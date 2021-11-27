973babb13126cb368116597ec5c7394b7808a5bb
PunitVJain <putwind@gmail.com>
2021-11-27 13:17:47 +0530

    commit

--- class_and_function/03.py
+++ class_and_function/03.py
@@ -13,3 +13,22 @@ print(mydict)
 # python practice 
 print('Python is my life for sometime', end='\n')
 #  python for learn python the hard way.
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        num = list(str(x))
+        if x in range(-2**31,2**31 - 1):
+            if num[0] == '-':
+                num.remove('-')
+                nrevs = int(''.join(num[::-1])) * -1
+                return nrevs
+            nrevs = int(''.join(num[::-1]))
+            return nrevs
+        return 0
+
+        
+  
+Sol = Solution()
+print(Sol.rev_num(321))
