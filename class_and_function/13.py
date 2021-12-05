00beaa6878f8bffa5a6dac747b141c1bc4055cd4
PunitVJain <putwind@gmail.com>
2021-12-05 18:18:53 +0530

    beautiful number

--- class_and_function/13.py
+++ class_and_function/13.py
@@ -8,7 +8,7 @@ def eqidx(mylist):
     return mylist
 mylist = [0, -7, 1, 5, 2, -4, 3, 0]
 
-print(eqidx(mylist))
+#print(eqidx(mylist))
 
 def febio(rt):
     fv, sv = 0, 1
@@ -18,4 +18,39 @@ def febio(rt):
     print()
 
 rt = 5
-febio(rt)
+#febio(rt)
+
+
+#----------------------------------
+
+def isbeautiful(N):
+    nums = list(str(N))
+    nums = list(map(int, nums))
+    print(nums)
+    mylist = []
+    for iteam in range(len(nums)):
+        if nums[iteam] == nums.count(nums[iteam]):
+            mylist.append(True)
+            #print(nums[iteam])
+        else:
+            #print(nums[iteam], nums.count(iteam))
+            mylist.append(False)
+    print(mylist)
+    return all(mylist)
+
+print(isbeautiful(333))
+
+
+#-------------------------------------------------
+
+
+
+
+N = 10
+buildings = {buld:height for buld in range(1,N)}
+
+
+
+
+
+
