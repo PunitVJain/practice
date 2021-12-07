8a487ff7b2de7c4c22a767084c7ac4dd4376ae94
Punit <punit.jain@exponentia.ai>
2021-12-07 11:25:55 +0530

    binary search requirement

---
+++ DATA_Structure_and_Algorithms/09.py
@@ -0,0 +1,8 @@
+#  binary search algorithm in python.
+
+# Binary search
+# 1. sort the given array in assending order.
+# 2. find the mid of the array.
+# 3. check the ele in the left half or right half of the array.
+# 4. repeat the same process till the time the element which is supposed to be searched is found.
+
