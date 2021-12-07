c0ded9ed7b26df6a63329420f406190be161e920
Punit <punit.jain@exponentia.ai>
2021-12-07 23:17:05 +0530

    diagonal inheritance

--- aproches/OOP/Sample.py
+++ aproches/OOP/Sample.py
@@ -1,6 +1,33 @@
 #  super class of the other classes
-
+#  learning inheritance in Python 
+#  diagonal inheritance.
 
 class Sample:
 
-    def __in
+    def __init__(self, name, mobno):
+        self.name = name
+        self.mobno = mobno
+    
+    def myname(self):
+        return "My Name is", self.name, "My Mobile Number is", self.mobno 
+    
+class Example(Sample):
+
+    def __init__(self, name, mobno):
+        super().__init__(name, mobno)
+
+class ExampleTwo(Sample):
+
+    def __init__(self, name, mobno):
+        super().__init__(name, mobno)
+
+class GerExample(Example, ExampleTwo):
+
+    def __init__(self, name, mobno):
+        super().__init__(name, mobno)
+    
+    def name_and_number(self):
+        return super().myname()
+
+gx = GerExample("Punit Jain", 8237914234)
+print(gx.name_and_number())
