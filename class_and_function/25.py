61743337ec410f2938f02467e7809c7f47ef7b11
PunitVJain <putwind@gmail.com>
2021-12-12 16:51:48 +0530

    int input

--- class_and_function/25.py
+++ class_and_function/25.py
@@ -2,7 +2,20 @@
 
 mydict = {'First_Name': "Punit", 'Last_Name': "Jain"}
 
-for iteam, value in mydict.items():
-    print(iteam, value)
+#for iteam, value in mydict.items():
+#    print(iteam, value)
 
 # item -->  is the function of the dictionary.
+
+
+#  hash() in python.
+
+mytuple =  (1, 2)
+
+print(hash(mytuple))
+
+
+# string to integer
+
+value = int(input(""))
+print(value)
