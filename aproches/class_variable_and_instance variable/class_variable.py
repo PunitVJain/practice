29f300b1fad2259703df3cf5825d3d1a35a5a3de
Punit <punit.jain@exponentia.ai>
2021-12-13 11:52:31 +0530

    instance varible in python

---
+++ aproches/class_variable_and_instance variable/class_variable.py
@@ -0,0 +1,34 @@
+#  class variables & instance variables.
+
+
+
+
+class SampleOne:
+
+    num = 10 
+
+    def __init__(self, first_name, last_name) -> None:
+        self.first_name = first_name # instance variable in python
+        self.last_name = last_name # instance variable in python
+    
+    def full_name(self):
+        return self.first_name + " " + self.last_name
+
+
+        
+
+sam_one = SampleOne("Punit", "Jain")
+sam_two = SampleOne("Geeta", "Jain")
+
+print(sam_one.first_name)
+print(sam_one.num)
+print(sam_one.full_name())
+print(sam_two.first_name)
+print(sam_two.full_name())
+# instance variables are the variables defined in constructor of the class.
+# instance variable create the copy for the each object.
+# if the changes made in one copy will not reflect in other copes of the instance variable.
+# instance variables can be accessed in class with self keyword & outside the class they can be accessed by object name
+# The method who access the instance variable is called instance method.
+
+
