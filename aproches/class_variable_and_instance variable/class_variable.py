ae90fce53fe5c0a0875281e9d03c411c02c7f678
Punit <punit.jain@exponentia.ai>
2021-12-13 14:16:02 +0530

    instance variable & class variable

--- aproches/class_variable_and_instance variable/class_variable.py
+++ aproches/class_variable_and_instance variable/class_variable.py
@@ -1,15 +1,14 @@
 #  class variables & instance variables.
 
-
-
-
 class SampleOne:
 
     num = 10 
-
+    
     def __init__(self, first_name, last_name) -> None:
-        self.first_name = first_name # instance variable in python
-        self.last_name = last_name # instance variable in python
+        if SampleOne.num <=16:
+            self.first_name = first_name # instance variable in python
+            self.last_name = last_name # instance variable in python
+        SampleOne.num += 1
     
     def full_name(self):
         return self.first_name + " " + self.last_name, self.num
@@ -19,16 +18,25 @@ class SampleOne:
 
 sam_one = SampleOne("Punit", "Jain")
 sam_two = SampleOne("Geeta", "Jain")
-
-print(sam_one.first_name)
+sam_three = SampleOne("Ram", "Jain")
+sam_four = SampleOne("Seeta", "Jain")
+sam_five = SampleOne("Lakshaman", "Jain")
+sam_six = SampleOne("Hanuman", "Jain")
+sam_seven = SampleOne("Ravan", "Jain")
+#print(sam_one.first_name)
 print(sam_one.num) # can access the class variable with class & instance of the class out side the class
 print(SampleOne.num) 
+print(sam_one.first_name)
+
 print(sam_one.full_name())
 print(sam_two.first_name)
 print(sam_two.full_name()) #  class variable & instance variable can be access with self inside the class
 print(sam_one.__dict__)
 print(sam_two.__dict__)
 print(SampleOne.__dict__)
+print(sam_one.__new__(SampleOne))
+
+
 
 # instance variables are the variables defined in constructor of the class.
 # instance variable create the copy for the each object.
