09d862404250ac7c019df57b1be83d0f1485179f
Punit <punit.jain@exponentia.ai>
2021-12-19 19:20:32 +0530

    febo

--- class_and_function/60.py
+++ class_and_function/60.py
@@ -14,12 +14,21 @@ def febo(limit):
 
 value = febo(10)
 
-print(value.__next__())
-print(value.__next__())
-print(value.__next__())
-print(value.__next__())
-print(value.__next__())
-print(value.__next__())
+#print(value.__next__())
+#print(value.__next__())
+#print(value.__next__())
+#print(value.__next__())
+#print(value.__next__())
 
+#======================
 
+# normal function with febonicie.
+
+def febonic(lim):
+    val, ele =  0, 1
+    while val < lim:
+        print(val, end=" ")
+        val, ele = ele, val + ele
+
+febonic(10)
 
