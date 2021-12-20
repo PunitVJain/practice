501aaa67dc3878575d52cd26dc4f98e0271cabaf
Punit <punit.jain@exponentia.ai>
2021-12-20 10:53:51 +0530

    commit

--- aproches/class_variable_and_instance variable/class_variable_part_2.py
+++ aproches/class_variable_and_instance variable/class_variable_part_2.py
@@ -2,12 +2,12 @@
 
 class SampleOne:
 
-    _num = 0
+    __num = 0
 
     def __new__(cls):
-        if cls._num > 2:
+        if cls.__num >= 2:
             raise Exception("Too many objs created.")
-        cls._num +=1
+        cls.__num +=1
         return super(SampleOne, cls).__new__(cls)
 
     def __init__(self) -> None:
@@ -16,8 +16,9 @@ class SampleOne:
 
 sam = SampleOne()
 sam1 = SampleOne()
-sam2 = SampleOne()
+#sam2 = SampleOne()
+#sam3 = SampleOne()
 
 print(sam.__dict__)
 print(sam1.__dict__)
-print(sam2.__dict__)
+#print(sam2.__dict__)
