3b2a7dcbcf228de868091d451e2c10db01e76dd8
PunitVJain <putwind@gmail.com>
2021-12-24 23:51:03 +0530

    generator function

--- aproches/generator_in_python/generators.py
+++ aproches/generator_in_python/generators.py
@@ -3,12 +3,19 @@
 def mygenerator():
     for iteam in range(100):
         yield iteam
-x = mygenerator()
-print(x.__next__())
-print(x.__next__())
-print(x.__next__())
-print(x.__next__())
-print(x.__next__())
+#x = mygenerator()
+
 #  as in python needed to use __next__()
 #  if there is yield in function this will be generator.
 
+def samgenerator():
+    co = 0
+    while True:
+        co += 1
+        yield co
+print(samgenerator().__next__())
+print(samgenerator().__next__())
+print(samgenerator().__next__())
+print(samgenerator().__next__())
+print(samgenerator().__next__())
+print(samgenerator().__next__())
