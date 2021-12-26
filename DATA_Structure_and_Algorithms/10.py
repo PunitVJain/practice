7bfd9d9e453800400322e61508267c4c52017e19
PunitVJain <putwind@gmail.com>
2021-12-26 15:45:11 +0530

    bubble sort algorithm in python

--- DATA_Structure_and_Algorithms/10.py
+++ DATA_Structure_and_Algorithms/10.py
@@ -1,19 +1,16 @@
 # Bubble sort algorithm in python.
 
 class BubbleSort:
-
-    def __init__(self, lst:list) -> None:
-        self.lst = lst
     
-    def doit(self):
-        for iteam in range(len(self.lst)):
-            for value in range(len(self.lst)-iteam-1):
-                if self.lst[value] > self.lst[value + 1]:
-                    self.lst[value], self.lst[value + 1] = self.lst[value+1], self.lst[value]
-        return self.lst
+    def doit(self, lst):
+        for iteam in range(len(lst)):
+            for value in range(len(lst)-iteam-1):
+                if lst[value] > lst[value + 1]:
+                    lst[value], lst[value + 1] = lst[value+1], lst[value]
+        return lst
 
 
 
 mylist = [2, 9, 1, 3, 5, 6, 4, 6, 4]
-bs = BubbleSort(mylist)
-print(bs.doit())
+bs = BubbleSort()
+print(bs.doit(mylist))
---
+++ DATA_Structure_and_Algorithms/11.py
@@ -0,0 +1,2 @@
+#  quick sort algorithm in python.
+
