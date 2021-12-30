dd52daf79d86e2331573ebc88b6760fe6f1a8daa
Punit <punit.jain@exponentia.ai>
2021-12-30 17:37:43 +0530

    link list

---
+++ Data Structure in python/LinkList.py
@@ -0,0 +1,15 @@
+# implementation of link list in python
+
+class Node:
+
+    def __init__(self, data = None, link = None) -> None:
+        self.data = data
+        self.link = link
+    
+class LinkList:
+
+    def __init__(self) -> None:
+        self.head = None
+
+
+
