a06b1b1a052066e089d8a66f3b057f1b1e9a6cbf
Punit <punit.jain@exponentia.ai>
2021-12-30 12:01:47 +0530

    module

--- requests_module/req.py
+++ requests_module/req.py
@@ -7,3 +7,11 @@ url = "https://www.google.com/"
 
 res = requests.get(url).text
 print(res)
+
+def find_data(url = url):
+    try:
+        res = requests.get(url).text
+        return res
+    except Exception as err:
+        return {"status": False, "msg": "Failed", "Error": err}, 200
+    
