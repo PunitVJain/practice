d5367fdee6a9ae8192a5d61d5ad186f7418d9cdb
Punit <punit.jain@exponentia.ai>
2021-12-30 16:45:35 +0530

    commit

--- practice_pandas & numpy/01.py
+++ practice_pandas & numpy/01.py
@@ -4,5 +4,4 @@ import pandas as pd
 data = {"first_name": ["Punit", "Ram"], "last_name": ["Jain", "Charan"], "mobile_no": ["9876543298", "8967452389"]}
 
 df = pd.DataFrame(data)
-print(df)
-
+#print(tuple(df.columns))
