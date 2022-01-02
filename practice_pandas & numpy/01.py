b7cd7809c7e2e2fbbb464362cd822cd926eb57ce
PunitVJain <putwind@gmail.com>
2022-01-02 15:24:51 +0530

    commi

---
+++ practice_pandas & numpy/01.py
@@ -0,0 +1,8 @@
+# pandas
+import pandas as pd
+
+data = {"first_name": ["Punit", "Ram"], "last_name": ["Jain", "Charan"], "mobile_no": ["9876543298", "8967452389"]}
+
+df = pd.DataFrame(data)
+print(tuple(df.columns))
+
