67839efef22ca0c51d3ae1643750c1d8c34b03f9
PunitVJain <putwind@gmail.com>
2022-01-23 11:40:13 +0530

    commit

--- class_in_python/first_day.py
+++ class_in_python/first_day.py
@@ -9,7 +9,7 @@ class SampleFirst:
         return self.__add__(one, two)
     
 sf = SampleFirst()
-print(sf.__hash__())
+#sprint(sf.__hash__())
 
 class SampleSecond:
 
@@ -18,3 +18,22 @@ class SampleSecond:
 
     def __repr__(self) -> str:
         pass
+
+
+class Solution:
+
+    def __init__(self,fname, lname) -> None:
+        self.fname = fname
+        self.lname = lname
+    
+    def fullname(self):
+        return self.fname + self.lname
+    
+
+solone =  Solution("Punit ", "Jain")
+soltwo = Solution("Ram ", "Jain")
+print(solone.fullname())
+print(soltwo.fullname())
+
+
+
