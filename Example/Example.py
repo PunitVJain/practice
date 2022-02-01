3d6866e0984baff99a76f6a44b60b19a64f9f905
PunitVJain <putwind@gmail.com>
2022-02-02 01:27:13 +0530

    commit

--- Example/Example.py
+++ Example/Example.py
@@ -10,3 +10,36 @@ print(id(mylst))
 mylst.append('Punit Jain')
 print(id(mylst))
 print(mylst)
+
+
+class SampleSolution:
+     
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def name_add(self):
+        return "Punit Jain from SampleSolution"
+
+class SampleSolutionOne:
+
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def name_add(self):
+        return "Punit Jain from SampleSolutionOne"
+
+class SampleSolutionTwo:
+
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def name_add(self):
+        return "Punit Jain from SampleSolutionTwo"
+
+
+class SampleSolutionAll(SampleSolution, SampleSolutionOne, SampleSolutionTwo):
+
+    def __init__(self) -> None:
+        super(SampleSolutionOne).__init__()
+ssa = SampleSolutionAll()
+print(super(SampleSolutionOne, ssa).name_add())
