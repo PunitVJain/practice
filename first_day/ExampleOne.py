55ea9cd4a6d23903d885b48364b64c34b4b6a0fa
PunitVJain <putwind@gmail.com>
2022-02-12 01:55:51 +0530

    commit

---
+++ first_day/ExampleOne.py
@@ -0,0 +1,13 @@
+#  python 
+
+class ExampleOne:
+
+    def __init__(self) -> None:
+        pass
+    
+    def full_name(self):
+        return None
+    
+    def mobile_no(self):
+        return None
+    
