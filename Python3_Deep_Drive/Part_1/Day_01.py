f5da2a9bb1888257c5c6350153a551c336a9f4a3
PunitVJain <putwind@gmail.com>
2022-02-16 14:27:47 +0530

    commit

--- Python3_Deep_Drive/Part_1/Day_01.py
+++ Python3_Deep_Drive/Part_1/Day_01.py
@@ -1,3 +1,16 @@
 # Zen of Python
 # PEP20 Documents
 
+class SampleOne:
+    pass
+
+#  multiple lines in python 
+#  explicit or implicit way for multiple lines in python.
+mylst = [1, 3, 4, 3, 2, 1,
+        5, 6, 7, 7,
+        6, 7, 8, 54]
+print(mylst)
+
+mytup = (2, 3, 2, 
+        4)
+print(mytup)
