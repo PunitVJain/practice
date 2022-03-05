c7a23f9ac682cb032f4bb653307430d279f6529b
PunitVJain <putwind@gmail.com>
2022-03-05 22:53:40 +0530

    commit

---
+++ Amazon_interview/05_03_2022_2.py
@@ -0,0 +1,13 @@
+#  class in python for link list.
+
+class ExampleOne:
+
+    def __init__(self) -> None:
+        pass
+
+class ExampleTwo:
+
+    def __init__(self) -> None:
+        pass
+
+    
