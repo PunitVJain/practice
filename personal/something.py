9ff889ad8ece73a4502fdf46c0d355152f519aa5
PunitVJain <putwind@gmail.com>
2022-03-10 00:10:18 +0530

    somthing

--- personal/something.py
+++ personal/something.py
@@ -1,5 +1,7 @@
 #  class for Punit Jain
 
+#  @property is usefull to getter & setter methods in Pythonic way.
+
 class Rectangle:
 
     def __init__(self, length, width) -> None:
@@ -8,8 +10,26 @@ class Rectangle:
 
     @property
     def length(self):
-        return self.length
+        return self._length
     
     @length.setter
     def length(self, other):
-        pass
+        if other <= 0:
+            raise "Values can't be Zero."
+        self._length = other
+    
+    @property
+    def width(self):
+        return self._width
+    
+    @width.setter
+    def width(self, other):
+        if other <= 0:
+            raise "Value can't be Zero."
+        self._width = other
+
+
+rec = Rectangle(10, 20)
+rec.length = 100
+rec.width = 200
+print(rec.length, rec.width)
