1851069c667be71dfe57f2c92aa1cdfecea51044
PunitVJain <putwind@gmail.com>
2022-03-18 20:31:16 +0530

    commit

---
+++ DATA_Structure_and_Algorithms/LinkList.py
@@ -0,0 +1,15 @@
+# python for linklist
+
+class Node:
+
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next_address = None
+
+class LinkList:
+
+    def __init__(self) -> None:
+        self.head = None
+    
+if __name__ == "__main__":
+    pass
