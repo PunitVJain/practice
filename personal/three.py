6f573f1751ee5be44e677f4d896adf19bff061c1
PunitVJain <putwind@gmail.com>
2022-03-19 18:38:22 +0530

    commit

--- personal/three.py
+++ personal/three.py
@@ -10,6 +10,15 @@ class SecondLargest:
     def __init__(self, lst:list) -> None:
         self.lst = lst
     
+    @property
+    def lst(self):
+        return self._lst
+    
+    @lst.setter
+    def lst(self):
+        pass
+
+    
     def __max_num(self):
         max_num = self.lst[0]
         for ele in self.lst:
@@ -20,8 +29,7 @@ class SecondLargest:
     def sec_lar(self):
         max_num = self.__max_num()
         mod_lst = self.lst.remove(max_num)
-        sec_lar_num = self.__max_num()
-        return sec_lar_num
+        return self.__max_num()
     
 if __name__ == "__main__":
     sl = SecondLargest([9, 2, 3, 5, 7, 8, 10, 6, 28, 29, 20])
