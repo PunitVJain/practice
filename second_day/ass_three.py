b0db5d36bf1f4dbc3101263dd640a1f7bd53ff9c
PunitVJain <putwind@gmail.com>
2022-04-01 21:12:29 +0530

    my commit

---
+++ second_day/ass_three.py
@@ -0,0 +1,28 @@
+# class In python
+
+class Rectangle:
+
+    def __init__(self) -> None:
+        pass
+
+
+class Cube(object):
+
+    def __init__(self, side):
+        self.side = side
+    
+    @property
+    def side(self):
+        return self._side
+    
+    @side.setter
+    def side(self, side):
+        if side <=0:
+            raise "Side can't be '-ve'."
+        self._side = side
+    
+    def __repr__(self) -> str:
+        return "Instance of class 'Cube'."
+    
+    
+    
